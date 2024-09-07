0b169b5a79fb22c8f6662731d7fdd1971171899d
nimba005 <nimbaoyugi@gmail.com>
2024-09-07 18:38:41 +0300

    Added invoice generator component form

---
+++ src/components/InvoiceGenerator.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <h1>Invoice Generator</h1>
+        <InvoiceForm @generate="generateInvoice" />
+        <InvoiceDisplay v-if="invoiceGenerated" :invoice="invoice" />
+    </div>
+</template>
+
+<script>
+import InvoiceForm from './InvoiceForm.vue';
+import InvoiceDisplay from './InvoiceDisplay.vue';
+
+export default {
+    components: {
+        InvoiceForm,
+        InvoiceDisplay
+    },
+    data() {
+        return {
+            invoiceGenerated: false,
+            invoice: {}
+        };
+    },
+    methods: {
+        generateInvoice(invoice) {
+            this.invoice = invoice;
+            this.invoiceGenerated = true;
+        }
+    }
+};
+</script>
+
+<style>
+/* Add some basic styling */
+h1 {
+    margin-bottom: 20px;
+}
+</style>
